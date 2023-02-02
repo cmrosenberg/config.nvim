9d6de89a1358bdcd1723a55e65d7eec781910e74
Carl Martin Rosenberg <github@cmrosenberg.com>
2023-02-02 10:07:44 +0100

    Clean up LSP module

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,45 +1,33 @@
+-- Uncomment the following line to enable debugging
+-- vim.lsp.set_log_level("debug")
 local lspconfig = require("lspconfig")
 
+local on_attach = function(client, bufnr)
+
+        local bufopts = {noremap=true, silent=true, buffer = bufnr}
+
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+        vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, bufopts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+        vim.keymap.set("n", "ge", vim.diagnostic.open_float, bufopts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+
+        vim.keymap.set("n", "<leader>df", vim.diagnostic.goto_next, bufopts)
+        vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, bufopts)
+
+        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, bufopts)
+        end
+
+
 lspconfig.gopls.setup{
-        on_attach = function()
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = 0})
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = 0})
-        vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, {buffer = 0})
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer = 0})
-
-        vim.keymap.set("n", "<leader>df", vim.diagnostic.goto_next, {buffer = 0})
-        vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {buffer = 0})
-
-        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer = 0})
-        end
-}
-
-lspconfig.pyright.setup{
-        on_attach = function()
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = 0})
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = 0})
-        vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, {buffer = 0})
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer = 0})
-
-        vim.keymap.set("n", "<leader>df", vim.diagnostic.goto_next, {buffer = 0})
-        vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {buffer = 0})
-
-        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer = 0})
-        end
+        on_attach = on_attach,
 }
 
 lspconfig.tsserver.setup{
-       on_attach = function()
-       vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = 0})
+        on_attach = on_attach,
+}
 
-       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = 0})
-       vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, {buffer = 0})
-
-       vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer = 0})
-
-       vim.keymap.set("n", "<leader>df", vim.diagnostic.goto_next, {buffer = 0})
-       vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {buffer = 0})
-
-       vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer = 0})
-       end
+lspconfig.pyright.setup{
+        on_attach = on_attach,
 }
