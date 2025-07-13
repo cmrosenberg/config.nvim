94dea85aa4cac9b7764734af830c41cc3988b6c3
Carl Martin Rosenberg <github@cmrosenberg.com>
2025-07-13 14:50:08 +0200

    BROKEN: commit erroneous attempt at listing plugins - might fix this later

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,20 +1,13 @@
-call plug#begin()
-Plug 'neovim/nvim-lspconfig'
+local vim = vim
+local Plug = vim.fn['plug#']
 
-Plug 'tpope/vim-commentary'
-Plug 'tpope/vim-eunuch'
-Plug 'tpope/vim-fugitive'
-
-Plug 'nvim-lua/plenary.nvim'
-Plug 'nvim-treesitter/nvim-treesitter'
-
-Plug 'nvim-telescope/telescope-fzf-native.nvim'
-
-Plug 'nvim-telescope/telescope.nvim', { 'tag': '0.1.8' }
-
-call plug#end()
-
-
-au BufEnter *.go nnoremap <localleader>p :%!gofmt<cr>
-au BufEnter *.go set noexpandtab
-au BufEnter *.go set tabstop=4 | set shiftwidth=4
+vim.call('plug#begin')
+Plug('neovim/nvim-lspconfig')
+Plug('tpope/vim-commentary')
+Plug('tpope/vim-eunuch')
+Plug('tpope/vim-fugitive')
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-treesitter/nvim-treesitter')
+Plug('nvim-telescope/telescope-fzf-native.nvim')
+Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.8' })
+vim.call('plug#end')
